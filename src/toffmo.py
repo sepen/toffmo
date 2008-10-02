030b42f6efd9e8a9e0554ba300860ccf62392632
Jose V Beneyto <sepen@crux.nu>
2008-10-02 22:43:25 +0000

    Cleaned up and bumped to 0.1
    git-svn-id: file:///home/sepen/devel/sepen/mikeux-svnrepos/projects/toffmo@13 1d032a62-85df-11dd-86ee-6f710e92d0c0

--- src/toffmo.py
+++ src/toffmo.py
@@ -12,22 +12,21 @@ import datetime
 
 class Toffmo:
 	
-	APP_NAME='toffmo'
-	APP_VERSION='0.1-rc1'
+	APP_NAME			=	'toffmo'
+	APP_VERSION			=	'0.1'
+	CONFIG_DIR			=	''
+	CONFIG_FILE			=	'toffmo.conf'
+	DATA_DIR			=	''
 	
-	CONFIG_DIR=''
-	CONFIG_FILE='toffmo.conf'
-	
-	DATA_DIR=''
-	
-	MONEY_PER_HOUR=10
-	MONEY_SUFFIX='EUR'
-	TIME_WORK_START='08:00'
-	TIME_WORK_STOP='17:30'
-	TIME_PAUSE_START='13:30'
-	TIME_PAUSE_STOP='14:30'
-	
-	STATUS='stalled'
+	MONEY_PER_HOUR		=	'10'
+	MONEY_SUFFIX		=	'EUR'
+	TIME_WORK_START		=	'08:00'
+	TIME_WORK_STOP		=	'17:30'
+	TIME_PAUSE_START	=	'13:30'
+	TIME_PAUSE_STOP		=	'14:30'
+
+	TIME				=	''
+	STATUS				=	''
 	
 	def parse_config_file(self):
 		cfg = ConfigParser.ConfigParser()
@@ -42,13 +41,11 @@ class Toffmo:
 		
 		return True
 	
-	def get_today_date(self):
-		return datetime.datetime.now().strftime("%H:%M - %d/%m/%Y")
-	
-	def get_elapsed_time(self):
+	def initialize_vars(self):
+		# actual data and time
 		date_now = datetime.datetime.now()
 		time_now = datetime.time(date_now.hour, date_now.minute, date_now.second)
-		
+		# time's from config values
 		time_work_start_list = self.TIME_WORK_START.split(':')
 		time_work_start = datetime.time(int(time_work_start_list[0]), int(time_work_start_list[1]))
 		time_work_stop_list = self.TIME_WORK_STOP.split(':')
@@ -57,86 +54,106 @@ class Toffmo:
 		time_pause_start = datetime.time(int(time_pause_start_list[0]), int(time_pause_start_list[1]))
 		time_pause_stop_list = self.TIME_PAUSE_STOP.split(':')
 		time_pause_stop = datetime.time(int(time_pause_stop_list[0]), int(time_pause_stop_list[1]))
-		
+		# timedeltas
 		t_now = datetime.timedelta(hours=time_now.hour, minutes=time_now.minute, seconds=time_now.second)
 		t_work_start = datetime.timedelta(hours=time_work_start.hour, minutes=time_work_start.minute, seconds=time_work_start.second)
 		t_pause_start = datetime.timedelta(hours=time_pause_start.hour, minutes=time_pause_start.minute, seconds=time_pause_start.second)
 		t_pause_stop = datetime.timedelta(hours=time_pause_stop.hour, minutes=time_pause_stop.minute, seconds=time_pause_stop.second)
-		
+		# set status
 		if time_work_start < time_now < time_pause_start:
-			self.STATUS='running'
-			#print "DEBUG> de manyanas y cobro"
-			t = t_now - t_work_start
+			self.STATUS='working'
+			self.TIME = t_now - t_work_start
 		elif time_pause_start < time_now < time_pause_stop:
-			self.STATUS='stalled'
-			#print "DEBUG> comiendo y no cobro"
-			t = t_pause_start - t_work_start
+			self.STATUS='stopped'
+			self.TIME = t_pause_start - t_work_start
 		elif time_pause_stop < time_now < time_work_stop:
-			self.STATUS='running'
-			#print "DEBUG> de tardes y cobro"
-			t = (t_now - t_pause_stop) + (t_pause_start - t_work_start)
+			self.STATUS='working'
+			self.TIME = (t_now - t_pause_stop) + (t_pause_start - t_work_start)
 		else:
-			self.STATUS='stalled'
-			#print "DEBUG> fuera de hora y no cobro"
-			t = t_now - t_now
-		return t
+			self.STATUS='stopped'
+			self.TIME = t_now - t_now
 
-	def get_today_money(self):
-		time = self.get_elapsed_time()
+	def get_date(self):
+		return datetime.datetime.now().strftime("%H:%M - %d/%m/%Y")
+	
+	def get_earned_money(self):
+		time = self.TIME
 		money_per_second = float(self.MONEY_PER_HOUR) / 3600
 		money = int(time.seconds * money_per_second)
 		return money
 	
 	def get_text_message(self):
-		message = "\n" + self.get_today_date() + "\n\n" \
-			+ "Hours: " + str(self.get_elapsed_time()) + " (" \
+		message = "\n" + self.get_date() + "\n\n" \
+			+ "Hours: " + str(self.TIME) + " (" \
 			+ self.MONEY_PER_HOUR + self.MONEY_SUFFIX + "/hour)\n" \
-			+ "Earned: " + str(self.get_today_money()) +  " " \
+			+ "Earned: " + str(self.get_earned_money()) +  " " \
 			+ self.MONEY_SUFFIX + "\n"
 		return message
+	
+	def get_image_file(self):
+		return self.DATA_DIR + "earn.jpg"
 	
 	def close_application(self, widget, event, data=None):
 		gtk.main_quit()
 		return False
 	
 	def __init__(self):
+		
 		# get values from config file
 		self.parse_config_file()
+		self.initialize_vars()
+		
 		# create the main window
 		window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 		window.connect("delete_event", self.close_application)
-		window.set_border_width(10)
-		window.set_size_request(205, 370) # width x height
+		window.set_border_width(5)
+		window.set_size_request(202, 372) # width x height
 		window.set_resizable(False)
-		window.set_title("toffmo")
+		window.set_title(self.APP_NAME)
+		
 		# create vbox
 		vbox = gtk.VBox(False, 0)
+		# add vbox to window
 		window.add(vbox)
 		vbox.show()
-		# add image to vbox
+		
+		# create a frame
+		frameU = gtk.Frame()
+		# create a image
 		image = gtk.Image()
-		image.set_from_file(self.DATA_DIR + "earn.jpg")
-		vbox.add(image)
+		file = self.get_image_file()
+		image.set_from_file(file)
+		# add image to frame
+		frameU.add(image)
 		image.show()
-		# add a status label to vbox
-		lbl_status = gtk.Label("status: " + self.STATUS)
-		lbl_status.show()
-		vbox.add(lbl_status)
-		# add frame and label to vbox
-		frame = gtk.Frame()
-		label = gtk.Label(self.get_text_message())
-		frame.add(label)
-		label.show()
-		vbox.add(frame)
-		frame.show()
+		# add frame to vbox
+		vbox.add(frameU)
+		frameU.show()
+		
+		# create a frame
+		frameD = gtk.Frame()
+		# create a label
+		message = gtk.Label(self.get_text_message())
+		# add label to frame
+		frameD.add(message)
+		message.show()
+		# add frame to vbox
+		vbox.add(frameD)
+		frameD.show()
+
+		# create a label
+		status = gtk.Label(self.STATUS)
+		status.set_justify(gtk.JUSTIFY_LEFT)
+		# add label to vbox
+		vbox.add(status)
+		status.show()
+		
 		# finally show the main window
 		window.show()
-		
-def main():
-    gtk.main()
-    return True
-  
+    
 if __name__ == "__main__":
   Toffmo()
-  main()
+  gtk.main()
 
+
+# End of file
